6b10e26e8994312600f95a69cae55da1822d344a
DolceVii <dolcevii@hotmail.com>
2019-07-30 16:44:29 +0300

    Update raspberry_2019-07-30.dump.sql

--- raspberry_2019-07-30.dump.sql
+++ raspberry_2019-07-30.dump.sql
@@ -23,8 +23,8 @@ CREATE TABLE 'active' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'userrfid
 ----
 -- Data dump for active, a total of 4 rows
 ----
-INSERT INTO "active" ("id","userrfid","activerfid","userdate","UserName") VALUES ('1','04 A7 10 82 E1 5E 84','1','26-07-2019 16:59:02','Γιώργος Κουρτίδης');
-INSERT INTO "active" ("id","userrfid","activerfid","userdate","UserName") VALUES ('2','04 75 54 3A 28 36 80','1','26-07-2019 17:00:38','Θεόδωρος Κοσμίδης');
-INSERT INTO "active" ("id","userrfid","activerfid","userdate","UserName") VALUES ('3','39 1E ED 63','0','26-07-2019 17:35:26','Μάριος Ζίκος');
-INSERT INTO "active" ("id","userrfid","activerfid","userdate","UserName") VALUES ('4','04 E3 A9 EA 06 5F 80','1','27-07-2019 19:17:27','Κώστας Λαφτσής');
+INSERT INTO "active" ("id","userrfid","activerfid","userdate","UserName") VALUES ('1','04 A7 10 82 E1 5E 80','1','26-07-2019 16:59:02','Γιώργος Κουρτίδης');
+INSERT INTO "active" ("id","userrfid","activerfid","userdate","UserName") VALUES ('2','04 75 54 3A 28 36 84','1','26-07-2019 17:00:38','Θεόδωρος Κοσμίδης');
+INSERT INTO "active" ("id","userrfid","activerfid","userdate","UserName") VALUES ('3','39 1E ED 60','0','26-07-2019 17:35:26','Μάριος Ζίκος');
+INSERT INTO "active" ("id","userrfid","activerfid","userdate","UserName") VALUES ('4','04 E3 A9 EA 06 5F 81','1','27-07-2019 19:17:27','Κώστας Λαφτσής');
 COMMIT;
